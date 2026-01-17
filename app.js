b4756bdeef3f2554bb531d94d4ee56e10fab058a
melvink1002-spec <melvink1002@gmail.com>
2026-01-16 20:16:17 -0500

    Declared variables and added local storage

---
+++ app.js
@@ -0,0 +1,19 @@
+let taskNameInput = document.getElementById("taskName");
+let categoryInput = document.getElementById("category");
+let deadlineInput = document.getElementById("deadline");
+let statusInput = document.getElementById("status");
+
+let addTaskBtn = document.getElementById("addTaskBtn");
+let taskList = document.getElementById("taskList");
+
+let filterStatus = document.getElementById("filterStatus");
+let filterCategory = document.getElementById("filterCategory");
+
+// Data
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
+
+
+
+
