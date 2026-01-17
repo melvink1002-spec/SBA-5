3aa1de08f335a51457ff58d6d801afa3ce22933d
melvink1002-spec <melvink1002@gmail.com>
2026-01-16 20:47:56 -0500

    Created add task and display task functions

--- app.js
+++ app.js
@@ -17,7 +17,49 @@ addTaskBtn.addEventListener("click", addTask);
 filterStatus.addEventListener("change", displayTasks);
 filterCategory.addEventListener("inout", displayTasks);
 
+// addTask function
+function addTask() {
+    let name = taskNameInput.value;
+    let category = categoryInput.value;
+    let deadline = deadlineInput.value;
+    let status = statusInput.value;
 
+    if (!name || !category || !deadline) {
+        alert("Please fill out all fields");
+        return;
+    }
+
+    let task = {
+        id: Date.now(),
+        name: name,
+        category: category,
+        deadline: deadline,
+        status: status
+    };
+
+    tasks.push(task);
+    saveTasks();
+    displayTasks();
+    clearForm();
+}
+
+function displayTasks() {
+    taskList.innerHTML = "";
+    checkOverdueTasks();
+
+    let statusFilter = filterStatus.value;
+    let categoryFilter = filterCategory.value.toLowerCase();
+
+    let filteredTasks = tasks.filter(function (task) {
+        let statusMatch =
+            statusFilter === "All" || task.status === statusFilter;
+
+        let categoryMatch =
+            task.category.toLowerCase().includes(categoryFilter);
+
+        return statusMatch && categoryMatch;
+    });
+}
 
 
 
